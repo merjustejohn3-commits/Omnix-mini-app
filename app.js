6d3bb16ae472438fb419eefc23e545d0bcc950d4
merjustejohn3-commits <merjustejohn3@gmail.com>
2026-08-18 13:39:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -44,11 +44,17 @@ document
     .addEventListener("click", () => {
 
 
+/
+
 // Rewarded interstitial
 
 show_11593326().then(() => {
     // You need to add your user reward function here, which will be executed after the user watches the ad.
     // For more details, please refer to the detailed instructions.
+    alert('You have seen an ad!');
+})
+
+        
         showScreen(homeScreen);
 
     });
